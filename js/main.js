73a3a1a60c488a25f914c9460ffbc87f463ec5cb
Kevin McIntyre <kevin@Kevins-MacBook-Pro.local>
2016-04-27 19:40:35 -0400

    added waypoint

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 var $body = $('body');
+var $way = $('.way');
 
-$(function() {
-    $('li a').click(function(e) {
-        var $this = $(this);
-        $this.closest('ul').find('.color').removeClass('color');
-        $this.parent().addClass('color');
-    });
+$(function () {
+  $('li a').click(function (e) {
+    var $this = $(this);
+
+    $this.closest('ul').find('.color').removeClass('color');
+    $this.parent().addClass('color');
+  });
 });
+
+$way.waypoint(function (direction) {
+	if (direction == 'down') {
+		$way.addClass('way-animate');
+	} else {
+		$way.removeClass('way-animate');
+	}
+}, { offset: '80%' });
